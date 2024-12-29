4ecfa134a06eb1aa4048a194e10563b3df191029
Megatogen <megatogen@yandex.ru>
2024-12-29 13:21:06 +0300

    Exit func, decoder func and .py

--- console_app/.py/Decoder/Vigener_decoder.py
+++ console_app/.py/Decoder/Vigener_decoder.py
@@ -1,19 +1,19 @@
 myAlphabet = 'abcdefghijklmnopqrstuvwxyzабвгдеёжзийклмнопрстуфхцчшщъыьэюя `1234567890-=~!@#№$%^&*()_+[]:";'+"'"+'<>?,./'
 print('Enter "EXIT" or "QUIT" to exit from programm. In other case strike any other key.')
 while True:
-    print("Enter string for encoding: ")
+    print("Enter string for decoding: ")
     inpt = input()
     if inpt=="EXIT" or inpt=="QUIT":
         break
     inpt = inpt.lower()
     print("Enter key: ")
     key = input().lower()
-    encoded = ""
+    decoded = ""
     i = 1
     for letter in inpt:
         st = myAlphabet.find(letter)
         keyNow = (i % len(key)) - 1
-        outpt = myAlphabet[(st + keyNow) % len(myAlphabet)]
-        encoded += outpt
+        outpt = myAlphabet[(st - keyNow) % len(myAlphabet)]
+        decoded += outpt
         i += 1
-    print("Encoded: ///" + encoded + "///")
+    print("Decoded: ///" + decoded + "///")
--- lib/vigenerscipher.py
+++ lib/vigenerscipher.py
@@ -13,3 +13,17 @@ class Vigener():
             encoded += outpt
             i += 1
         return encoded
+    def decode(self, inpt, key="a"):
+        #Don't support \ symbol
+        myAlphabet = 'abcdefghijklmnopqrstuvwxyzабвгдеёжзийклмнопрстуфхцчшщъыьэюя `1234567890-=~!@#№$%^&*()_+[]:";'+"'"+'<>?,./'
+        inpt = inpt.lower()
+        key = key.lower()
+        decoded = ""
+        i = 1
+        for letter in inpt:
+            st = myAlphabet.find(letter)
+            keyNow = (i % len(key)) - 1
+            outpt = myAlphabet[(st + keyNow) % len(myAlphabet)]
+            decoded += outpt
+            i += 1
+        return decoded
